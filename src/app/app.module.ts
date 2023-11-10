0a1b91f9dee3753da05c6311541180bf6d2fc4b3
Bautista Madruga <bmadruga@wilab.io>
2023-11-09 22:41:43 -0300

    add interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoaderComponent } from './core/loader/loader.component';
-import { HttpClientModule } from '@angular/common/http';
 import { ResourceModule } from '@ngx-resource/handler-ngx-http';
 import { CoreModule } from './core/core.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AppHttpInterceptor } from './core/interceptors/app-http.interceptor';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { CoreModule } from './core/core.module';
     ResourceModule.forRoot()
 
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AppHttpInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
