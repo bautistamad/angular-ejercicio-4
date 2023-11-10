077f2a08e6963fe59977a2314daaa87f8d43be08
Bautista Madruga <bmadruga@wilab.io>
2023-11-09 22:15:04 -0300

    trying to fix loader

--- src/app/main/resolvers/persona.resolver.ts
+++ src/app/main/resolvers/persona.resolver.ts
@@ -3,6 +3,7 @@ import { IPersona } from '../api/models/i-persona';
 import { inject } from '@angular/core';
 import { PersonaResourceService } from '../api/resources/persona-resource.service';
 
+
 export const personaResolver: ResolveFn<IPersona[]> = (route, state) => {
   return inject(PersonaResourceService).get()
 };
